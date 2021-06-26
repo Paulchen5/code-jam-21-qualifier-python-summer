040c16686deab5406f5fe1bfc1b9b32c77438930
Paulchen <78027149+Paulchen5@users.noreply.github.com>
2021-06-26 18:32:33 +0200

    Test Sequence
    added test sequence

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -70,3 +70,38 @@ def make_table(rows: List[List[Any]], labels: Optional[List[Any]] = None, center
         table_body += "│\n"
     
     return top_line + table_header + table_body + bot_line
+
+table = make_table(
+    rows=[
+        ["Lemon"],
+        ["Sebastiaan"],
+        ["KutieKatj9"],
+        ["Jake"],
+        ["Not Joe"]
+    ]
+)
+print(table)
+
+table = make_table(
+    rows=[
+        ["Lemon", 18_3285, "Owner"],
+        ["Sebastiaan", 18_3285.1, "Owner"],
+        ["KutieKatj", 15_000, "Admin"],
+        ["Jake", "MoreThanU", "Helper"],
+        ["Joe", -12, "Idk Tbh"]
+    ],
+    labels=["User", "Messages", "Role"]
+)
+print(table)
+
+table = make_table(
+   rows=[
+       ["Ducky Yellow", 3],
+       ["Ducky Dave", 12],
+       ["Ducky Tube", 7],
+       ["Ducky Lemon", 1]
+   ],
+   labels=["Name", "Duckiness"],
+   centered=True
+)
+print(table)
